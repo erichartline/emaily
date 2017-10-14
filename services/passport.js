5b5d35bd84a0746f57be062549b782d0c67b727d
wildlifehexagon <crossthebreeze@gmail.com>
2017-10-14 13:12:19 -0500

    add proxy and refactor with async/await

--- services/passport.js
+++ services/passport.js
@@ -26,16 +26,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          // we already have a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          // we don't have a record with this ID, make a new record
-          new User({ googleId: profile.id }).save().then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // we already have a record with the given profile ID
+        return done(null, existingUser);
+      }
+        // we don't have a record with this ID, make a new record
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
     }
   )
 );

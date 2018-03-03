f0d5d470c8d7e47b6c36e2c0798f71f6a9a57e4d
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-03 15:26:07 -0600

    add form styling

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,8 +1,9 @@
 // SurveyForm shows a form for a user to add input
+import _ from "lodash"
 import React, { Component } from "react"
 import { reduxForm, Field } from "redux-form"
+import { Link } from "react-router-dom"
 import SurveyField from "./SurveyField"
-import _ from "lodash"
 
 const FIELDS = [
   { label: "Survey Title", name: "title" },
@@ -30,7 +31,13 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className=" teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     )

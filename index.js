ed736fc630cb04edc03de5a98f5e8b15a92a20d8
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-02 23:51:25 -0600

    set up survey post route

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(passport.session())
 // pass in app to our routing files
 require("./routes/authRoutes")(app)
 require("./routes/billingRoutes")(app)
+require("./routes/surveyRoutes")(app)
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets

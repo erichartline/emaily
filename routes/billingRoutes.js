eab62d6ec223143d911b56792a9e07c1aae2bb25
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-02 21:26:45 -0600

    add credits to user

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -10,6 +10,10 @@ module.exports = app => {
       source: req.body.id
     })
 
-    console.log(charge)
+    req.user.credits += 5
+    const user = await req.user.save()
+
+    // send user data back
+    res.send(user)
   })
 }
